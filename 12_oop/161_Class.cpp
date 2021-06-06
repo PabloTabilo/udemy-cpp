3beb5bf3a680c39d2f4092948fc3f8839f1a5a76
pablo.tabilo <pablo.tabilo@ticnow.cl>
2021-06-06 14:49:53 -0400

    oop

---
+++ 12_oop/161_Class.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<iterator>
+#include<stack>
+#include<set>
+#include<unordered_map>
+#include<math.h>
+#include<limits>
+#include<iomanip>
+#include<sstream>
+
+
+#define MEM(a, b) memset(a, (b), sizeof(a))
+
+
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int ll;
+typedef unsigned long long int uint64;
+typedef long double ld80;
+
+
+using namespace std;
+
+
+int gcd(int a, int b){
+    if(b==0)
+        return a;
+    return gcd(b,a%b);
+}
+
+class Rectangle{
+    private:
+        int l, r;
+    public:
+        Rectangle(int l = 0, int r = 0){
+            setL(l);
+            setR(r);
+        }
+        Rectangle(Rectangle &rect){
+            l = rect.getL();
+            r = rect.getR();
+        }
+        int area(){return l*r;}
+        int perimeter(){return 2*l+2*r;}
+        void setL(int l){this->l = (l>0?l:0);}
+        void setR(int r){this->r = (r>0?r:0);}
+        int getL(){return l;}
+        int getR(){return r;}
+};
+
+int main(){
+    Rectangle r1(10,5); // create on stack
+    Rectangle r2(r1);
+    cout<<r1.area()<<endl;
+    cout<<r2.area()<<endl;
+    Rectangle* p = new Rectangle(); // pointer heap
+    return 0;
+}
